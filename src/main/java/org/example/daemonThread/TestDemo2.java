762ac64ab4e3d54000067e338868af21df905f52
wuyl199588 <15715847275@163.com>
2020-11-09 17:09:04 +0800

    git第三次提交

--- src/main/java/org/example/daemonThread/TestDemo2.java
+++ src/main/java/org/example/daemonThread/TestDemo2.java
@@ -1,15 +1,13 @@
 package org.example.daemonThread;
 
 public class TestDemo2 {
-    /**
-     * 因为主线程迅速的结束了 守护线程还在睡眠1000ms中 所以守护线程也随之结束 没有写入文件
-     */
+
     public static void main(String[] args) throws InterruptedException {
         Runnable tr = new TestRunnable();
         Thread thread = new Thread(tr);
         thread.setDaemon(true); //设置守护线程
         thread.start(); //开始执行分进程
-//        Thread.sleep(2000);
+        Thread.sleep(2000);
     }
 
 }
